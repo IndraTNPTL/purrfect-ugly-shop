f06480d3ced01bf2b20b2afb08d532adbc3c5c68
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2023-05-16 11:22:31 +0200

    add-to-cart btn on all list pages

--- src/Pages/AllElectronics.jsx
+++ src/Pages/AllElectronics.jsx
@@ -3,57 +3,62 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function AllElectonics({ handleAddToCart }) {
-  const [electronics, setElectonics] = useState();
-  useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products/category/electronics")
-      .then((response) => {
-        setElectonics(response.data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
+	const [electronics, setElectonics] = useState();
+	useEffect(() => {
+		axios
+			.get("https://fakestoreapi.com/products/category/electronics")
+			.then((response) => {
+				setElectonics(response.data);
+			})
+			.catch((e) => console.log(e));
+	}, []);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  if (!electronics) {
-    return <p>Loading...</p>;
-  }
+	if (!electronics) {
+		return <p>Loading...</p>;
+	}
 
-  return (
-    <div className="page-container">
-      <div className="list-content">
-        <h1>
-          <span className="highlight-color italic">Purrfect</span>
-          <span className="italic"> Ugly </span>Electronics
-        </h1>
-        <div className="cards-wrapper">
-          {electronics.map((electronic) => (
-            <Link key={electronic.id} to={`/products/${electronic.id}`}>
-              <div className="card">
-                <div className="card-image">
-                  <img src={electronic.image} alt={electronic.name} />
-                </div>
-                <div className="card-content">
-                  <p className="product-name">{electronic.title}</p>
-                  <p className="price">{electronic.price}€</p>
-                  <div className="add-to-cart">
-                    <button
-                      className="btn-add-to-cart"
-                      onClick={() => handleAddToCart(electronic)}
-                    >
-                      {""}Add to cart{""}
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="page-container">
+			<div className="list-content">
+				<h1>
+					<span className="highlight-color italic">Purrfect</span>
+					<span className="italic"> Ugly </span>Electronics
+				</h1>
+				<div className="cards-wrapper">
+					{electronics.map((electronic) => (
+						<div key={electronic.id} className="card">
+							<Link to={`/products/${electronic.id}`}>
+								<div className="card-image">
+									<img
+										src={electronic.image}
+										alt={electronic.name}
+									/>
+								</div>
+								<div className="card-content">
+									<p className="product-name">
+										{electronic.title}
+									</p>
+									<p className="price">{electronic.price}€</p>
+								</div>
+							</Link>
+							<div className="add-to-cart">
+								<button
+									className="btn-add-to-cart"
+									onClick={() => handleAddToCart(electronic)}
+								>
+									Add to cart
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AllElectonics;
--- src/Pages/AllJewelery.jsx
+++ src/Pages/AllJewelery.jsx
@@ -3,58 +3,64 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function AllJewelery({ handleAddToCart }) {
-  const [jeweleries, setJewelery] = useState();
-  useEffect(() => {
-    axios
-      .get("https://fakestoreapi.com/products/category/jewelery")
-      .then((response) => {
-        setJewelery(response.data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
+	const [jeweleries, setJewelery] = useState();
+	useEffect(() => {
+		axios
+			.get("https://fakestoreapi.com/products/category/jewelery")
+			.then((response) => {
+				setJewelery(response.data);
+			})
+			.catch((e) => console.log(e));
+	}, []);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  if (!jeweleries) {
-    return <p>Loading...</p>;
-  }
+	if (!jeweleries) {
+		return <p>Loading...</p>;
+	}
 
-  return (
-    <div className="page-container">
-      <div className="list-content">
-        <h1>
-          <span className="highlight-color italic">Purrfect</span>
-          <span className="italic"> Ugly </span>Jewelery
-        </h1>
+	return (
+		<div className="page-container">
+			<div className="list-content">
+				<h1>
+					<span className="highlight-color italic">Purrfect</span>
+					<span className="italic"> Ugly </span>Jewelery
+				</h1>
 
-        <div className="cards-wrapper">
-          {jeweleries.map((jewelery) => (
-            <Link key={jewelery.id} to={`/products/${jewelery.id}`}>
-              <div className="card">
-                <div className="card-image">
-                  <img src={jewelery.image} alt={jewelery.name} />
-                </div>
-                <div className="card-content">
-                  <p className="product-name">{jewelery.title}</p>
-                  <p className="price">{jewelery.price}€</p>
-                  <div className="add-to-cart">
-                    <button
-                      className="btn-add-to-cart"
-                      onClick={() => handleAddToCart(jewelery)}
-                    >
-                      {""}Add to cart{""}
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="cards-wrapper">
+					{jeweleries.map((jewelery) => (
+						<div key={jewelery.id} className="card">
+							<Link to={`/products/${jewelery.id}`}>
+								<div className="card-image">
+									<img
+										id="jewelery-special-img-class"
+										src={jewelery.image}
+										alt={jewelery.name}
+									/>
+								</div>
+								<div className="card-content">
+									<p className="product-name">
+										{jewelery.title}
+									</p>
+									<p className="price">{jewelery.price}€</p>
+								</div>
+							</Link>
+							<div className="add-to-cart">
+								<button
+									className="btn-add-to-cart"
+									onClick={() => handleAddToCart(jewelery)}
+								>
+									Add to cart
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default AllJewelery;
